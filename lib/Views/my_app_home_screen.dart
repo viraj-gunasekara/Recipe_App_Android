07be474532b76ee9841d4f6a2f5884ca4c4f0403
viraj-gunasekara <vsgunasekara.dev@gmail.com>
2025-07-04 20:59:49 +0530

    home layout: banner

--- lib/Views/my_app_home_screen.dart
+++ lib/Views/my_app_home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_viraj/Utils/constants.dart';
+import 'package:flutter_app_viraj/Widget/banner.dart';
 import 'package:flutter_app_viraj/Widget/my_icon_button.dart';
 import 'package:iconsax/iconsax.dart';
 
@@ -20,31 +21,27 @@ class _MyAppHomeScreenState extends State<MyAppHomeScreen> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Padding(padding: EdgeInsets.symmetric(horizontal: 15),
-              child: Column(children: [
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
                 headerParts(),
-                Padding(padding: EdgeInsets.symmetric(vertical: 22),
-                child: TextField(
-                  decoration: InputDecoration(
-                    filled: true,
-                    prefixIcon: const Icon(Iconsax.search_normal),
-                    fillColor: Colors.white,
-                    border: InputBorder.none,
-                    hintText: "Search any recipes",
-                    hintStyle: TextStyle(
-                      color: Colors.grey,
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide.none,
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide.none,
+                mySearchBar(),
+                // banner
+                const BannerToExplore(),
+                const Padding(padding: EdgeInsets.symmetric(
+                  vertical: 20,
+                ),
+                  child: Text(
+                    "Categories",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
-                ),
-              ],),
+                // categories
+              ],
+              ),
             ),
           ],
         ),
@@ -53,9 +50,34 @@ class _MyAppHomeScreenState extends State<MyAppHomeScreen> {
     );
   }
 
+  Padding mySearchBar() {
+    return Padding(padding: EdgeInsets.symmetric(vertical: 22),
+              child: TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  prefixIcon: const Icon(Iconsax.search_normal),
+                  fillColor: Colors.white,
+                  border: InputBorder.none,
+                  hintText: "Search any recipes",
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide.none,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+              );
+  }
+
   Row headerParts() {
     return Row(children: [
-                Text("What are you\nCooking Today?", style: TextStyle(fontSize: 32,fontWeight: FontWeight.bold, height: 1,
+                Text("Easy Recipes for\nBusy days.", style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold, height: 1,
                   ),
                  ),
                 const Spacer(),
