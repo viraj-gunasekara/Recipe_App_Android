aaaabb06a42d1818b2c7d325b96a4bea3e1412dc
viraj-gunasekara <vsgunasekara.dev@gmail.com>
2025-07-04 22:53:14 +0530

    home layout: category list taken from firestoreDb

--- lib/Views/my_app_home_screen.dart
+++ lib/Views/my_app_home_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_app_viraj/Utils/constants.dart';
 import 'package:flutter_app_viraj/Widget/banner.dart';
 import 'package:flutter_app_viraj/Widget/my_icon_button.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class MyAppHomeScreen extends StatefulWidget {
   const MyAppHomeScreen({super.key});
@@ -12,76 +13,156 @@ class MyAppHomeScreen extends StatefulWidget {
 }
 
 class _MyAppHomeScreenState extends State<MyAppHomeScreen> {
+  String category = "All";
+  final CollectionReference categoriesItems = FirebaseFirestore.instance
+      .collection("Recipe-Category");
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kBackgroundColor,
-      body: SafeArea(child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(padding: EdgeInsets.symmetric(horizontal: 15),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                headerParts(),
-                mySearchBar(),
-                // banner
-                const BannerToExplore(),
-                const Padding(padding: EdgeInsets.symmetric(
-                  vertical: 20,
-                ),
-                  child: Text(
-                    "Categories",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    headerParts(),
+                    mySearchBar(),
+                    // banner
+                    const BannerToExplore(),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 20),
+                      child: Text(
+                        "Categories",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                     ),
-                  ),
+                    // categories
+                    selectedCategory(),
+                  ],
                 ),
-                // categories
-              ],
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-       ),
       ),
     );
   }
 
-  Padding mySearchBar() {
-    return Padding(padding: EdgeInsets.symmetric(vertical: 22),
-              child: TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  prefixIcon: const Icon(Iconsax.search_normal),
-                  fillColor: Colors.white,
-                  border: InputBorder.none,
-                  hintText: "Search any recipes",
-                  hintStyle: TextStyle(
-                    color: Colors.grey,
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide.none,
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide.none,
+  Widget selectedCategory(){
+    return StreamBuilder(
+      stream: categoriesItems.snapshots(),
+      builder:
+          (
+          context,
+          AsyncSnapshot<QuerySnapshot> streamSnapshot,
+          ) {
+        if (streamSnapshot.hasData) {
+          return SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: List.generate(
+                streamSnapshot.data!.docs.length,
+                    (index) => GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      category ==
+                          streamSnapshot
+                              .data!
+                              .docs[index]["name"];
+                    });
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(
+                        25,
+                      ),
+                      color:
+                      category ==
+                          streamSnapshot
+                              .data!
+                              .docs[index]["name"]
+                          ? kPrimaryColor
+                          : Colors.white,
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 10,
+                    ),
+                    margin: const EdgeInsets.only(
+                      right: 20,
+                    ),
+                    child: Text(
+                      streamSnapshot
+                          .data!
+                          .docs[index]["name"],
+                      style: TextStyle(
+                        color:
+                        category ==
+                            streamSnapshot
+                                .data!
+                                .docs[index]["name"]
+                            ? Colors.white
+                            : Colors.grey.shade600,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
                   ),
                 ),
               ),
-              );
+            ),
+          );
+        }
+        return Center(child: CircularProgressIndicator());
+      },
+    );
+  }
+
+  Padding mySearchBar() {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 22),
+      child: TextField(
+        decoration: InputDecoration(
+          filled: true,
+          prefixIcon: const Icon(Iconsax.search_normal),
+          fillColor: Colors.white,
+          border: InputBorder.none,
+          hintText: "Search any recipes",
+          hintStyle: TextStyle(color: Colors.grey),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide.none,
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide.none,
+          ),
+        ),
+      ),
+    );
   }
 
   Row headerParts() {
-    return Row(children: [
-                Text("Easy Recipes for\nBusy days.", style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold, height: 1,
-                  ),
-                 ),
-                const Spacer(),
-                MyIconButton(icon: Iconsax.notification, pressed: (){},)
-              ],);
+    return Row(
+      children: [
+        Text(
+          "Easy Recipes for\nBusy days.",
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+            height: 1,
+          ),
+        ),
+        const Spacer(),
+        MyIconButton(icon: Iconsax.notification, pressed: () {}),
+      ],
+    );
   }
 }
